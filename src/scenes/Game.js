867e1ab7bad685aedb3c2f94eb269edf4a5ddb90
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2021-02-03 12:05:20 +0100

    ajout du mouvement

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,6 +2,10 @@ import Phaser from '../lib/phaser.js';
 
 export default class Game extends Phaser.Scene
 {
+    player
+    platforms
+    cursors
+
     constructor()
     {
         super('game')
@@ -10,35 +14,89 @@ export default class Game extends Phaser.Scene
     preload()
     {
         this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
+        this.load.image('ground', 'assets/ground.png');
+        this.load.image('platform', 'assets/platform.png')
         this.load.image('star', 'assets/star.png');
         this.load.image('bomb', 'assets/bomb.png');
         this.load.spritesheet('dude',
         'assets/dude.png',
         { frameWidth: 32, frameHeight: 48 },
         );
+
+        //toujours à la fin
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     create()
     {
         this.add.image(400, 300, 'sky');
-        this.add.image(400, 568, 'ground').setScale(2);
+        const ground = this.physics.add.staticImage(400, 575, 'ground');
 
-        const platforms = this.physics.add.staticGroup();
+        this.platforms = this.physics.add.staticGroup();
 
         for(let i = 1; i < 4; ++i)
         {
             const x = Phaser.Math.Between(50, 750);
             const y = 150 * i;
 
-            const platform = platforms.create(x, y, 'ground');
+            const platform = this.platforms.create(x, y, 'platform');
             const body = platform.body;
             body.updateFromGameObject();
-        }
+        };
+
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+        
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.physics.add.collider(this.player, ground);
+        this.physics.add.collider(this.player, this.platforms);
     }
 
     update()
     {
+        if (this.cursors.left.isDown)
+        {
+            this.player.setVelocityX(-160);
 
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play('right', true);
+        }
+        else
+        {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down)
+        {
+            this.player.setVelocityY(-330);
+        }
     }
 }
